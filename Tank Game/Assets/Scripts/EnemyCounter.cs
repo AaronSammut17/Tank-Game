2f602ed4e5f63204576f6b57c2f5918a0ed02af9
AaronSammut17 <32704546+AaronSammut17@users.noreply.github.com>
2020-01-29 16:02:35 +0100

    Uplouding the files
    the last level is nearly done need to update the level manager.
    Then copy for the rest of the levels.

---
+++ Tank Game/Assets/Scripts/EnemyCounter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyCounter : MonoBehaviour
+{
+    public static int enemyCount = 0;
+    Text enemy;
+
+    void Start()
+    {
+        enemy = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemy.text = "Enemies: "+enemyCount+" / 22";
+    }
+}
